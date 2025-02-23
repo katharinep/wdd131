b9c1b86c2eee83b4fb2bdfeb14f79dad7e4e47aa
Katharine <katharine.risingphoenix@gmail.com>
2025-02-22 23:26:24 -0700

    Partial completion of assignment (some debugging needed)

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,74 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
-	}
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
+		}
 ]
+
+const articleContainer = document.querySelector(".review-content");
+
+articles.forEach((article) => {
+	const newArticle = document.createElement("article");
+	newArticle.classList.add("review-content");
+	
+	const bookTitle = document.createElement("h2");
+	bookTitle.id = "book-title";
+	bookTitle.textContent = article.title;
+
+	const bookImage = document.createElement("img");
+	bookImage.id = "post-image";
+	bookImage.src = article.imgSrc;
+	bookImage.alt = article.imgAlt;
+
+	const reviewParagraph = document.createElement("p");
+	reviewParagraph.id = "review-paragraph";
+	reviewParagraph.textContent = article.description;
+
+	newArticle.appendChild(bookTitle);
+	newArticle.appendChild(bookImage);
+	newArticle.appendChild(reviewParagraph);
+
+	articleContainer.appendChild(newArticle);
+});
+
+const detailContainer = document.querySelector("#metadata");
+
+articles.forEach((article) => {
+	const newArticle = document.createElement("article");
+	newArticle.classList.add("metadata");
+
+	const reviewDate = document.createElement("h3");
+	reviewDate.id = "date";
+	reviewDate.textContent = article.date;
+	
+	const bookAge = document.createElement("p");
+	bookAge.id = "ages";
+	bookAge.textContent = article.ages;
+
+	const bookGenre = document.createElement("p");
+	bookGenre.id = "genre";
+	bookGenre.textContent = article.genre;
+
+	const bookStars = document.createElement("p");
+	bookStars.id = "stars";
+	bookStars.textContent = article.stars;
+
+	newArticle.appendChild(reviewDate);
+	newArticle.appendChild(bookAge);
+	newArticle.appendChild(bookGenre);
+	newArticle.appendChild(bookStars);
+
+	detailContainer.appendChild(newArticle);
+});
