8720dfde02df69daa86b2c3a845f45d0ab28d2a9
Katharine <katharine.risingphoenix@gmail.com>
2025-03-03 19:01:04 -0700

    Finished assignment. Trying to fix link.

--- register/register.js
+++ register/register.js
@@ -43,6 +43,26 @@ function participantTemplate(count) {
     `;
 }
 
+function totalFees() {
+    let total = 0;
+    // the selector below lets us grab any element that has an id that begins with "fee"
+    let feeElements = document.querySelectorAll("[id^=fee]");
+    console.log(feeElements);
+    // querySelectorAll returns a NodeList. It's like an Array, but not exactly the same.
+    // The line below is an easy way to convert something that is list-like to an actual Array so we can use all of the helpful Array methods...like reduce
+    // The "..." is called the spread operator. It "spreads" apart the list, then the [] we wrapped it in inserts those list items into a new Array.
+    feeElements = [...feeElements];
+    // sum up all of the fees. Something like Array.reduce() could be very helpful here :) Or you could use a Array.forEach() as well.
+    // Remember that the text that was entered into the input element will be found in the .value of the element.
+    
+    feeElements.forEach(input => {
+        total += Number(input.value) || 0;
+    
+    });
+    
+    return total;
+}
+
 console.log(participantTemplate(2));
 
 
@@ -57,24 +77,12 @@ document.addEventListener("DOMContentLoaded", () => {
         addButton.insertAdjacentHTML("beforebegin", newParticipantHTML);
     });
 
+    let form = document.querySelector("form");
+
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        let totalFee = 0;
-        function totalFees() {
-            // the selector below lets us grab any element that has an id that begins with "fee"
-            let feeElements = document.querySelectorAll("[id^=fee]");
-            console.log(feeElements);
-            // querySelectorAll returns a NodeList. It's like an Array, but not exactly the same.
-            // The line below is an easy way to convert something that is list-like to an actual Array so we can use all of the helpful Array methods...like reduce
-            // The "..." is called the spread operator. It "spreads" apart the list, then the [] we wrapped it in inserts those list items into a new Array.
-            feeElements = [...feeElements];
-            // sum up all of the fees. Something like Array.reduce() could be very helpful here :) Or you could use a Array.forEach() as well.
-            // Remember that the text that was entered into the input element will be found in the .value of the element.
-            
-            // once you have your total make sure to return it!
-            
-            };
+        let totalFee = totalFees();
 
         let adultName= document.getElementById("adult_name").value.trim();
 
