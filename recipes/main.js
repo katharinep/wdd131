942986813eef47ece3be07a216b8ce7ce823ef37
Katharine <katharine.risingphoenix@gmail.com>
2025-03-15 20:24:35 -0600

    Partial recipe template

--- recipes/main.js
+++ recipes/main.js
@@ -1,6 +1,7 @@
 //import recips.mjs//
 import recipes from './recipes.mjs';
-console.log(recipes);
+//console.log(recipes);//
+
 //random function//
 function randomNumber(num) {
 	let random = Math.floor(Math.random()*(num-1));
@@ -13,9 +14,41 @@ function generateRecipe(recipes) {
 	return recipes[randomRecipe];
 }
 
-console.log(generateRecipe(recipes));
-//create template function in HTML//
+//console.log(generateRecipe(recipes));//
 
+//create template function in HTML//
+function recipeTemplate(recipe) {
+	return `<section id="recipes">
+            <img src="${recipe.image}" alt="${recipe.name}">
+            <article class="recipe">
+                
+                <div class="tags">
+					${recipe.tags.map(tag => `<span class="tag">${tag}</span>`)}
+                </div>
+                
+                <h2>${recipe.name}</h2>
+                
+                <span
+	                class="rating"
+	                role="img"
+	                aria-label="Rating: ${recipe.rating} out of 5 stars"
+                >
+	                <span aria-hidden="true" class="icon-star">⭐</span>
+	                <span aria-hidden="true" class="icon-star">⭐</span>
+	                <span aria-hidden="true" class="icon-star">⭐</span>
+	                <span aria-hidden="true" class="icon-star-empty">⭐</span>
+	                <span aria-hidden="true" class="icon-star-empty">☆</span>
+                </span>
+                
+                <p class="recipe-description">
+                    ${recipe.description}
+                </p>
+            </article>
+        </section>
+	`;
+}
+
+console.log(recipeTemplate(generateRecipe(recipes)));
 //template function to generate markup to display tags (takes list of tags as parameter)//
 
 //template function for rating stars, takes numbers as a parameter for number of stars to display//
