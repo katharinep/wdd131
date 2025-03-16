8ba1277fa153778ec689d4de856fa589af454770
Katharine <katharine.risingphoenix@gmail.com>
2025-03-15 20:45:53 -0600

    Partial tag and rating functions

--- recipes/main.js
+++ recipes/main.js
@@ -21,9 +21,9 @@ function recipeTemplate(recipe) {
 	return `<section id="recipes">
             <img src="${recipe.image}" alt="${recipe.name}">
             <article class="recipe">
-                
+
                 <div class="tags">
-					${recipe.tags.map(tag => `<span class="tag">${tag}</span>`)}
+					${tagsTemplate(recipe.tags)}
                 </div>
                 
                 <h2>${recipe.name}</h2>
@@ -49,10 +49,33 @@ function recipeTemplate(recipe) {
 }
 
 console.log(recipeTemplate(generateRecipe(recipes)));
+
 //template function to generate markup to display tags (takes list of tags as parameter)//
+function tagsTemplate(tags) {
+	return tags.map(tag => `<span class="tag">${tag}</span>`).join('');
+}
 
 //template function for rating stars, takes numbers as a parameter for number of stars to display//
+function ratingTemplate(rating) {
+	let html = `<span
+	class="rating"
+	role="img"
+	aria-label="Rating: ${recipe.rating} out of 5 stars"
+>`
+	for (let i = 1; i <= 5; i++) {
+		if (i <= rating) {
+			html += `<span aria-hidden="true" class="icon-star">⭐</span>`;
+		} else {
+			html += `<span aria-hidden="true" class="icon-star-empty">☆</span>`;
+		}
+	}
 
+	html += `</span>`
+	return html;
+}
+
+const recipe = generateRecipe(recipes);
+console.log(recipeTemplate(recipe));
 //create init function to run when the page loades to render out a random recipe.//
 
 //test page//
