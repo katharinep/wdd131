1c10cf1d65eae3787680a9db57f184cad58af623
Katharine <katharine.risingphoenix@gmail.com>
2025-04-05 21:59:57 -0600

    Most of article function written

--- genealogy/main.js
+++ genealogy/main.js
@@ -1,2 +1,27 @@
 import { articles } from './data.js';
 import { forms } from './data.js';
+
+const articleList = document.getElementById('articles');
+
+function renderArticles(data) {
+    data.forEach(article => {
+        const post = document.createElement('article');
+        post.clasList.add('post');
+        post.innerHTML = `
+            <div class="post-detail">
+                <p class="post-date">${article.date}</p>
+                <p class="post-author">${article.author}</p>
+                <p class="post-tags">Tags: <span class=tag>census</span></p>
+            </div>
+            <div class="post-content">
+                <h2 class="post-title">${article.title}</h2>
+                <img src="${article.img}" alt="${article.alt}" class="archive material">
+                <p class="post-body">${article.content}</p>
+            </div>`;
+        articles.appendChild(post);
+    });
+}
+
+function renderForms() {
+
+}
